b77cfee4987acf12e76d0ef5fd5c196e71800b78
pranay2244 <pranaykumarbommaraju2000@gmail.com>
2021-06-09 01:05:20 +0530

    Pull Request

--- scripts/query.js
+++ scripts/query.js
@@ -7,3 +7,25 @@ $(document).ready(function() {
 function register() {
     window.location = "/aicongress/register.html";
 }
+
+var acc = document.getElementsByClassName("accordion");
+var pan = document.getElementsByClassName("drop-down");
+let i;
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = pan[this.id];
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+        for (let j = 0; j < acc.length; j++) {
+            if (this.id != j) {
+                pan[j].style.display = "none";
+                acc[j].classList.remove("active");
+            }
+        }
+    });
+
+}
